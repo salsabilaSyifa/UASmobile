38ca7223610fd6f662ffda4f4a65fb9f177c43c5
Anis Hanifa <101486425+anishanifa@users.noreply.github.com>
2022-06-07 19:56:48 +0800

    update data class

--- app/src/main/java/com/example/poemy/network/Poetry.kt
+++ app/src/main/java/com/example/poemy/network/Poetry.kt
@@ -13,3 +13,10 @@ data class Poetry1(
     val lines : List<String>,
     val linecount : Int
 )
+
+data class Poetry2(
+    val title : String,
+    val author : String,
+    val lines : List<String>,
+    val linecount : Int
+)
